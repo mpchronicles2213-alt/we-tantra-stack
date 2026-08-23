81c4ef6137fc7e192dbde3d285ac363e5d3ce500
TantraStack <mpchronicles2213@gmail.com>
2026-08-23 17:04:23 +0530

    Refactor pino-http import syntax

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,10 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import * as pinoHttpModule from "pino-http";
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
-
-const pinoHttp = (pinoHttpModule as any).default || pinoHttpModule;
 
 const app: Express = express();
 
@@ -27,6 +25,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
